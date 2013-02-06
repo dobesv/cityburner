4d060ecc8c012a9f4f124d1f3a434d80e7a4ff42
Dobes Vandermeer <dobesv@gmail.com>
2013-02-06 12:03:30 -0800

    Updated to use the new type annotations feature in Elm

--- cityburner.elm
+++ cityburner.elm
@@ -2,54 +2,73 @@ import Automaton (init')
 import JavaScript
 import JavaScript.Experimental
 
-foreign import jsevent "provideRobotAnimation"
-    (castElementToJSElement (spacer 0 0))
-    jsRobotAnimation :: Signal JSElement
+jsRobotAnimation : Signal JSElement
+foreign import jsevent "provideRobotAnimation" (castElementToJSElement (spacer 0 0)) jsRobotAnimation : Signal JSElement
 
 robotAnimation = lift (castJSElementToElement 262 373) jsRobotAnimation
 
+-- Return Just head for a non-empty list, or Nothing for an empty list
+headMaybe : [x] -> Maybe x
+headMaybe lst =
+    case lst of
+        [] -> Nothing
+        x::xs -> Just x
+
 -- Find the nth element of a list, or Nothing if the list is too short
+nthMaybe : Int -> [x] -> Maybe x
 nthMaybe n lst =
     case lst of
         [] -> Nothing
-        x:xs -> if n == 0 then Just x else nthMaybe (n-1) xs
+        x::xs -> if n == 0 then Just x else nthMaybe (n-1) xs
 
 -- Get the nth member of a list; throw an error if the list is too short
+nth : Int -> [x] -> x
 nth n lst = head (drop n lst)
 
 -- Copy elements from the given list in sequence until p returns false for an element.
+takeWhile : (x -> Bool) -> [x] -> [x]
 takeWhile p lst =
     case lst of
         [] -> []
-        x:xs -> if p x then x : (takeWhile p xs) else []
+        x::xs -> if p x then x :: (takeWhile p xs) else []
+
 -- Skip elements until p returns false, then return the remaining elements
+dropWhile : (x -> Bool) -> [x] -> [x]
 dropWhile p lst =
     case lst of
         [] -> []
-        x:xs -> if p x then (dropWhile p xs) else lst
+        x::xs -> if p x then (dropWhile p xs) else lst
 
 -- Reverse composition
+-- >>> : (a -> b) -> (b -> c) -> (a -> c)
 f >>> g = g . f
 
 -- Pipeline operator
+-- |> : a -> (a -> b) -> b
 x |> g = g x
 
 -- half
+half : a -> a
 half x = x `div` 2
 
 -- Extract part of an image
+cropImage : [Char] -> Int -> Int -> Int -> Int -> Int -> Int -> Element
 cropImage url origWidth origHeight left top width height =
   collage width height
     [sprite url origWidth origHeight (origWidth `div` 2 - left, origHeight `div` 2 - top)]
 
 -- Extract part of an element
+crop : Element -> Int -> Int -> Int -> Int -> Element
 crop element left top width height =
   let eltWidth = widthOf element
       eltHeight = heightOf element
   in collage width height
     [toForm (eltWidth `div` 2 - left, eltHeight `div` 2 - top) element]
 
+bldgSheet : {width:Int,height:Int,src:[Char]}
 bldgSheet = {width=1800,height=400,src="images/CityBurner_Buildings_V2.png"}
+type Bldg = {width:Int,height:Int,image:Element,hitMask:Form}
+bldg : (Int,Int,Int,Int,[(Int,Int)]) -> Bldg
 bldg (left,top,width,height,hitMask) =
   {width = width, height = height,
    image = cropImage bldgSheet.src bldgSheet.width bldgSheet.height left top width height,
@@ -67,15 +86,24 @@ b9=(1206,228,133,172,[(1208,400), (1210,243), (1234,243), (1234,229), (1245,229)
 ba=(1374,25,90,375,  [(1376,400), (1376,299), (1382,270), (1383,166), (1386,166), (1386,136), (1409,89), (1414,86), (1415,25), (1423,87), (1433,87), (1440,122), (1445,122), (1462,197), (1462,209), (1457,214), (1457,263), (1462,267), (1462,290), (1457,296), (1458,348),(1463,351),(1463,400)])
 bb=(1504,294,140,106,[(1509,400), (1509,337), (1513,337), (1515,317), (1576,313), (1579,316), (1579,357), (1583,355), (1586,319), (1630,318), (1630,337), (1641,336), (1641,400)])
 bc=(1656,324,134,76, [(1660,400), (1660,368), (1667,362), (1665,341), (1687,326), (1704,342), (1724,328), (1746,342), (1762,325), (1785,343), (1785,400)])
+bldgs : [Bldg]
 bldgs = map bldg [b1,b2,b3,b4,b5,b6,b7,b8,b9,ba,bb,bc]
 
+bldgCount : Int
 bldgCount = length bldgs
+
+allBldgsWidth : Int
 allBldgsWidth = foldr (\bldg w -> bldg.width + w) 0 bldgs
 
+firePattern : [Int]
 firePattern = [9,3,10,2,7,2,8,2,6,1,4,1,9,2,6,1,8,3,7,1]
+firePatternLength : Int
 firePatternLength = length firePattern
+fireAngles : [Float]
 fireAngles = map (\a -> a * 0.04 * pi) [4,7,2,9,5,4,3,5,3,2,4,2,3,1,9,4,2,5,1,4,3,2]
+fireAnglesLength : Int
 fireAnglesLength = length fireAngles
+firePolygon : Int -> Int -> Int -> Form
 firePolygon x y scale =
     let n = (x+y) `div` 10
         firePolys n angle =
@@ -84,9 +112,10 @@ firePolygon x y scale =
                     dx = (cos angle) * r
                     dy = (sin angle) * r
                     deltaAngle = nth (n `mod` fireAnglesLength) fireAngles
-                in (dx,dy) : (firePolys (n+1) (angle+deltaAngle))
+                in (dx,dy) :: (firePolys (n+1) (angle+deltaAngle))
     in filled fireRed $ polygon (firePolys n 0) (x,y)
 
+nthBuilding : Int -> Bldg
 nthBuilding n = case (nthMaybe (n `mod` bldgCount) bldgs) of
     Just b -> b
     Nothing -> head bldgs
@@ -96,6 +125,12 @@ nthBuilding n = case (nthMaybe (n `mod` bldgCount) bldgs) of
 laserRed = rgba 200 50 50 0.75
 fireRed = rgba 200 50 50 0.3
 
+type Fire = {worldX:Int,worldY:Int,since:Int}
+
+type GameState = {oldFires:[Fire]}
+type JSElts = {robot:Element}
+type GameInput = {t:Int, mouseDown:Bool, mousePosition:(Int,Int), jselts:JSElts}
+updateGame : GameInput -> GameState -> (Element,GameState)
 updateGame input oldState =
     let {t,mouseDown,mousePosition} = input
         oldFires = oldState.oldFires
@@ -111,13 +146,13 @@ updateGame input oldState =
                     if bX - loopX > gameWidth then ([],[])
                     else case bs of
                             [] -> if n < 10 then buildingsOnScreen loopX bX bldgs (n+1) else ([],[])
-                            x:xs -> let rest = buildingsOnScreen loopX (bX + x.width) xs n
+                            x::xs -> let rest = buildingsOnScreen loopX (bX + x.width) xs n
                                     in if ((bX + x.width) < loopX) then rest
                                        else let bldgLeft = bX - loopX
                                                 bldgTop = gameHeight - x.height
                                                 form = toForm (bldgLeft + (half x.width),bldgTop + (half x.height)) x.image
                                                 hitMask = move bldgLeft bldgTop x.hitMask
-                                            in (form : (fst rest), hitMask : (snd rest))
+                                            in (form :: (fst rest), hitMask :: (snd rest))
             in buildingsOnScreen currentLoopX 0 bldgs 1
         headCenter = (292,410)
         monsterShapes = [robotElt]
@@ -137,7 +172,6 @@ updateGame input oldState =
 
         fireRadius since = (sqrt (t - since))*speed
         fireVisible {worldX,since} = worldX > distanceTravelled - (fireRadius since)
-
         fireShape {worldX,worldY,since} =
             let radius = fireRadius since
                 x = worldX - distanceTravelled
@@ -150,17 +184,18 @@ updateGame input oldState =
         mouseOverBuilding = any (isWithin mousePosition) cityHitMasks
         mouseOverFire = any (isWithin mousePosition) (map fireShape oldVisibleFires)
         timeSinceLastFire = case oldFires of
-            f:fs -> t - f.since
+            f::fs -> t - f.since
             [] -> 1000
 
         newFires = if mouseDown && mouseOverBuilding && (not mouseOverFire) && (timeSinceLastFire > 333)
-                   then newFire : oldVisibleFires else oldVisibleFires
+                   then newFire :: oldVisibleFires else oldVisibleFires
 
         newState = {oldFires=newFires}
         fireShapes = map fireShape newFires
 
         shapes = monsterShapes ++ laserShapes ++ cityShapes ++ fireShapes
         gameView = collage gameWidth gameHeight shapes
+
         debugView () =
             let debugTextView = show >>> toText >>> (Text.color green) >>> Text.monospace >>> Graphics.text
                 views = [debugTextView input,
@@ -173,10 +208,15 @@ updateGame input oldState =
 
     in (newView,newState)
 
+mkjselts : Element -> JSElts
 mkjselts r = {robot=r}
+jselts : Signal JSElts
 jselts = lift mkjselts robotAnimation
+mkinput : Int -> Bool -> (Int,Int) -> JSElts -> GameInput
 mkinput t mouseDown mousePosition jselts = let r = {t=t, mouseDown=mouseDown, mousePosition=mousePosition, jselts=jselts} in r
+input : Signal GameInput
 input = lift4 mkinput (every 0.1) Mouse.isDown Mouse.position jselts
-startState = {oldFires=[],buildings=[]}
+startState : GameState
+startState = {oldFires=[]}
 gameAutomaton = init' startState updateGame
 main = Automaton.run gameAutomaton input
